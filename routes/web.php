fd12d87e9d408a13f6ec1b47586666c8790047cb
somleff <dmytro.somliev@ibm.com>
2021-01-18 08:24:28 +0100

    (feat) - Blueprint of Posts CRUD.

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
 });
@@ -20,3 +9,13 @@ Route::get('/', function () {
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
+
+Route::get('posts', 'PostController@index')->name('posts.index');
+Route::get('posts/create', 'PostController@create')->name('posts.create');
+Route::get('posts/{post}', 'PostController@show')->name('posts.show');
+Route::post('posts', 'PostController@store')->name('posts.store');
+
+Route::get('posts/{post}/edit', 'PostController@edit')->name('posts.edit');
+
+Route::put('posts/{post}', 'PostController@update')->name('posts.update');
+Route::delete('/posts/{post}', 'PostController@destroy')->name('posts.destroy');
