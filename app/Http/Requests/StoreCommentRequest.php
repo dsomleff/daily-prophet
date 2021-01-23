d5fa35956ccb9c8d870d4949271018fcbfe46aff
somleff <dmytro.somliev@ibm.com>
2021-01-23 10:30:16 +0100

    (feat) - Comment Validation.

--- app/Http/Requests/StoreCommentRequest.php
+++ app/Http/Requests/StoreCommentRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class PostRequest extends FormRequest
+class StoreCommentRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -24,8 +24,7 @@ class PostRequest extends FormRequest
     public function rules()
     {
         return [
-            'title' => ['required', 'min:3', 'max:255'],
-            'context' => ['required', 'min:10']
+            'body' => ['required', 'min:10']
         ];
     }
 }
